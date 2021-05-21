0bcdb15b9e63d76827bb27e7487a4a5ef5d5d70f
Nina NJul <nina.dev.web@gmail.com>
2021-05-21 20:57:29 +0300

    add component

--- index.js
+++ index.js
@@ -1,38 +1,5 @@
 import React from 'react';
 import ReactDom from 'react-dom';
+import Header from './components/Header';
 
-// const app = document.getElementById('app');
-
-// const h1 = document.createElement('h1');
-// h1.textContent = 'My Text';
-// h1.id = 'title';
-// h1.className = 'red';
-// app.appendChild(h1);
-
-/*
-const h1 = React.createElement(
-  'h1',
-  { id: 'title', className: 'red' },
-  'My Text'
-);
-*/
-
-/*
-const h1 = (
-  <h1 id='title' className='red'>
-    My Text
-  </h1>
-);
-*/
-
-function Header() {
-  return (
-    <h1 id='title' className='red'>
-      My Text
-    </h1>
-  );
-}
-
-// ReactDom.render(h1, app);
-// ReactDom.render(Header(), app);
-ReactDom.render(Header(), document.getElementById('app'));
+ReactDom.render(<Header />, document.getElementById('app'));
